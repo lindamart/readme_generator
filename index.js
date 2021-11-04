d0633146087a05e44955742ec9e942be723ce025
Linda Martinez <lindanmjw@gmail.com>
2021-11-03 21:37:38 -0400

    deleted node folder

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = [
+const headings = [
     {
         type: "input",
-        message: "What is your name?",
-        name: "name",
+        message: "Please give a desription of your application",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "Write your Table of contents",
+        name: "contents",
+        choices: ["*[Installation](#installation)", "* [License](#license)", "License", "Contributing", "Tests", "Questions", ]
     },
     {
         type: "input",
-        message: "What is your location?",
-        name: "location",
+        message: "Advise on what to do to install",
+        name: "install",
     },
     {
         type: "input",
-        message: "Please give a bio",
-        name: "bio",
+        message: "Please provide app usage",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Please provide applications license",
+        name: "license",
+        // update list
+        choices: ["Installation", "Usage", "License", "Contributing", "Tests", "Questions", ]
     },
     {
         type: "input",
-        message: "Please provide your LinkedIn URL",
-        name: "linkedin",
+        message: "Please advise on contributions",
+        name: "contributing",
     },
     {
         type: "input",
-        message: "Please provide your Github URL",
-        name: "github",
+        message: "Please advise on test details",
+        name: "test",
     },
+    {
+        type: "input",
+        message: "Contact info for questions",
+        name: "contactEmail",
+    },
+    {
+        type: "input",
+        message: "Project Title",
+        name: "title",
+    },
+        {
+            type: "input",
+            message: "GitHub User Name",
+            name: "githubName",
+        },
+        {
+            type: "input",
+            message: "GitHub profile link",
+            name: "githubLink",
+        },
+        
 ];
 
-inquirer.prompt(questions).then((response) => {
+inquirer.prompt(headings).then((response) => {
     const content = `
 <!DOCTYPE html>
 <html lang="en">
@@ -38,30 +72,23 @@ inquirer.prompt(questions).then((response) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>${response.title}</title>
 </head>
 <body>
-    <h1>${response.name}</h1>
-</body>
-</html>
-
-----------------------
-
 
 # Test
 ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
-​
+
 ## Description
 ​
-This tests homework
+${response.description}
 ​
 ## Table of Contents 
+​${response.contents}
+
 ​
-* [Installation](#installation)
-​
-* [Usage](#usage)
-​
-* [License](#license)
+
+
 ​
 * [Contributing](#contributing)
 ​
@@ -73,9 +100,9 @@ This tests homework
 ​
 To install necessary dependencies, run the following command:
 ​
-```
+
 npm i
-```
+
 ​
 ## Usage
 ​
@@ -93,17 +120,19 @@ Send money.
 ​
 To run tests, run the following command:
 ​
-```
+
 npm test
-```
+
 ​
 ## Questions
 ​
 If you have any questions about the repo, open an issue or contact me directly at amaddatu@gmail.com. You can find more of my work at [amaddatu](https://github.com/amaddatu/).
 
+</body>
+</html>
 `;
 
-    fs.writeFile("portfolio.html", content, (err) => {
+    fs.writeFile("readme.md", content, (err) => {
         if (err) {
             console.error(err);
             return;
